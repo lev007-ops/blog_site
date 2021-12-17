3951ecc508ac57e366836594870f9721ea276b8e
ms lev <lev_bariakh@icloud.com>
2021-12-17 14:08:18 +0500

    update post model

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cre_date', 'was_published_recently')
+    fieldsets = (
+        (None, {
+            "fields": (
+                'title', 'text'
+            ),
+        }),
+        ('Date info', {'fields': ('cre_date',)})
+    )
+
+    list_filter = ('cre_date',)
+    search_fields = ('title', 'text')
+
+
+admin.site.register(Post, PostAdmin)
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,12 +1,23 @@
-from django.db import models
+from datetime import timedelta
 
+from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 
 class Post(models.Model):
     title = models.CharField(verbose_name='Название поста', max_length=110)
     text = models.TextField(verbose_name='Текст поста', max_length=400)
-    date_created = models.DateTimeField(verbose_name='Дата публикиции', default=timezone.now())
+    cre_date = models.DateTimeField(verbose_name='Дата публикиции', default=timezone.now())
 
     def __str__(self) -> str:
         return self.title
+
+    @admin.display(
+        boolean=True,
+        ordering='cre_date',
+        description='Опубликовано недавно?'
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - timedelta(days=1) <= self.cre_date <= now
