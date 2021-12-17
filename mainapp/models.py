0f383be1cf597c179893cf24485933cb17c11822
ms lev <lev_bariakh@icloud.com>
2021-12-17 13:42:33 +0500

    create post model

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from django.utils import timezone
+
+
+class Post(models.Model):
+    title = models.CharField(verbose_name='Название поста', max_length=110)
+    text = models.TextField(verbose_name='Текст поста', max_length=400)
+    date_created = models.DateTimeField(verbose_name='Дата публикиции', default=timezone.now())
+
+    def __str__(self) -> str:
+        return self.title
